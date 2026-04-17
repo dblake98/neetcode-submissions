36ba09dcc4a0bd1684f56795031fbd22171441c1
Darion B. <50345746+dblake98@users.noreply.github.com>
2026-04-17 12:05:26 -0700

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.java
@@ -0,0 +1,29 @@
+class MinStack {
+
+    private LinkedList<Integer> stack;
+    private LinkedList<Integer> minStack;
+
+    public MinStack() {
+        this.stack = new LinkedList();
+        this.minStack = new LinkedList();
+    }
+    
+    public void push(int val) {
+        stack.addFirst(val);
+        Integer currentMin = minStack.isEmpty() ? val : Math.min(val, minStack.getFirst());
+        minStack.addFirst(currentMin);
+    }
+    
+    public void pop() {
+        stack.removeFirst();
+        minStack.removeFirst();
+    }
+    
+    public int top() {
+        return stack.getFirst();
+    }
+    
+    public int getMin() {
+        return minStack.getFirst();
+    }
+}
